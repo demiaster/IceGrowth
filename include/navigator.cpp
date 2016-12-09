17b9485240d185f861bb6d11332912e3c269fde7
demiaster <marta.celeste.feriani@gmail.com>
2016-12-09 02:07:58 +0100

    icegenerator.cpp: modified dla_pattern to use navigator class

--- include/navigator.cpp
+++
@@ -1,6 +0,0 @@
-#include "navigator.h"
-
-Navigator::Navigator()
-{
-
-}
--- src/icegenerator.cpp
+++ src/icegenerator.cpp
@@ -1,5 +1,6 @@
 #include "icegenerator.h"
 #include "point.h"
+#include "navigator.h"
 
 #include "image.h"
 
@@ -7,6 +8,7 @@
 #include <random>
 #include <algorithm>
 #include <sstream>
+#include<iostream>
 
 #define HITTEMPERATURE -100
 #define DIFFUSE_K 0.2
@@ -19,7 +21,9 @@ namespace controller
 
     IceGenerator::IceGenerator(const std::size_t _width,
                     const std::size_t _height) :
-        m_heatGrid(_width, _height), m_iceGrid(_width, _height)
+        m_heatGrid(_width, _height),
+        m_iceGrid(_width, _height),
+        m_image(_width, _height)
     {
         m_width = _width;
         m_height = _height;
@@ -33,7 +37,7 @@ namespace controller
         // set the initial seed of the iceGrid + set the temperature of the seed inside the heatgrid
         m_iceGrid.freeze(m_width / 2, m_height / 2, 1.0);
         m_heatGrid.setTemperature(m_width / 2, m_height / 2, HITTEMPERATURE);
-
+        m_image.clearScreen(0, 0, 0);
         return;
     }
 
@@ -41,7 +45,6 @@ namespace controller
     {
         m_heatGrid.diffuse(DIFFUSE_K, DIFFUSE_TIME);
         m_iceGrid.merge(m_heatGrid);
-        //DLA here
         this->dla_pattern();
 
         return;
@@ -55,41 +58,72 @@ namespace controller
 
     void IceGenerator::dla_pattern()
     {
-        std::random_device rd; // obtain a random number from hardware
-        std::mt19937 eng(rd()); // seed the generator
+        /*
         std::uniform_int_distribution<> distr(-1, 1); // define the range
         std::uniform_int_distribution<> offdistr(0, 2 * (m_width + m_height) - 4);
         std::uniform_real_distribution<> pdistrib(0, 1.0);
+        */
+        model::Navigator navigator (m_width, m_height);
 
-        std::size_t offset = offdistr(eng);
+        //set seed
+        m_image.setPixel(m_width / 2, m_height / 2, 255, 255, 255);
 
-        model::Point random_walker = (offset < m_width) ? model::Point{offset, 0} :
-                                  (m_width <= offset && offset < m_width + m_height) ? model::Point{W - 1, offset - m_width} :
-                                  (m_width + m_height <= offset && offset < 2 * m_width + m_height) ?
-                                                                            model::Point{offset - m_width - m_height, m_height - 1} :
-                                                                            model::Point{0, offset - m_width - m_width - m_height};
-
-        int j = 0;
-        while(true)
+        for( int i = 0; i < 1000; ++i)
         {
-           random_walker.x = random_walker.x + distr(eng);
-           random_walker.y = random_walker.y + distr(eng);
+            //calculating a random index on the frame of the image
+            //std::size_t offset = offdistr(eng);
 
-           //only check upper bounds because of
-           //Point unsigned int x, y coordinates
-           if(random_walker.x >= m_width || random_walker.y >= m_height)
-           {
-               break;
-           }
-           PERCENTAGE probability = m_iceGrid.hit(random_walker.x, random_walker.y);
-           if(probability > pdistrib(eng))
-           {
-               m_iceGrid.freeze(random_walker.x, random_walker.y, probability);
+            //converting from that index to the actual x, y values for the random walker
+            /*model::Point random_walker = (offset < m_width) ? model::Point{offset, 0} :
+                                      (m_width <= offset && offset < m_width + m_height) ? model::Point{m_width - 1, offset - m_width} :
+                                      (m_width + m_height <= offset && offset < 2 * m_width + m_height) ?
+                                                                                model::Point{offset - m_width - m_height, m_height - 1} :
+                                                                                model::Point{0, offset - m_width - m_width - m_height};
 
-               break;
-           }
-           ++j;
+            */
+            model::Point random_walker = navigator.setOnBorders();
+            m_image.setPixel(random_walker.x, random_walker.y, 255, 0, 0);
+
+            int j = 0;
+            while(true)
+            {
+               navigator.walk(random_walker);
+               std::cout << "x, y (" << random_walker.x << ", " << random_walker.y << ")\n";
+
+               //random_walker.x = random_walker.x + distr(eng);
+               //random_walker.y = random_walker.y + distr(eng);
+
+               //only check upper bounds because of
+               //Point unsigned int x, y coordinates
+               if(random_walker.x >= m_width || random_walker.y >= m_height)
+               {
+                   break;
+               }
+               //PERCENTAGE probability = m_iceGrid.hit(random_walker.x, random_walker.y);
+               else if(m_image.hit(random_walker.x, random_walker.y, 255, 255, 255))
+               {
+                   //ONLY TO TEST AGAIN NORMAL BEHAVIOUR
+                   //std::cout << "x, y (" << random_walker.x << ", " << random_walker.y << ")\n";
+                   m_image.setPixel(random_walker.x,
+                                  random_walker.y,
+                                  255, 255, 255);
+                   break;
+               }
+               //TODO: figure out why this breaks everything
+               /*if(probability > pdistrib(eng))
+               {
+                   m_iceGrid.freeze(random_walker.x, random_walker.y, probability);
+                   m_image.setPixel(random_walker.x,
+                                  random_walker.y,
+                                  255, 255, 255);
+                   break;
+               }
+               */
+               ++j;
+               m_image.setPixel(random_walker.x, random_walker.y, 255 - (j % 255), 0, 0);
+            }
         }
+        m_image.save("dla_controller_navigator.png");
         return;
     }
 }
