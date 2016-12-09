24dbef1ea4c9141a30c38859511881caf4726d7e
demiaster <marta.celeste.feriani@gmail.com>
2016-12-09 02:07:12 +0100

    navigator.*: implemented setOnBorder() and walk functions

--- include/navigator.h
+++ include/navigator.h
@@ -1,11 +1,27 @@
 #ifndef NAVIGATOR_H
 #define NAVIGATOR_H
 
+#include <random>
+#include "point.h"
 
-class Navigator
+namespace model
 {
-public:
-    Navigator();
-};
+    class Navigator
+    {
+    public:
+        Navigator(std::size_t _width, std::size_t _height);
+        model::Point setOnBorders();
+        void walk(model::Point& _walker);
+        void diffuseOnAxis();
+
+    private:
+        std::random_device rd; // obtain a random number from hardware
+        std::mt19937 eng; // seed the generator
+        std::uniform_int_distribution<> distr;
+        std::uniform_int_distribution<> offdistr;
+        std::uniform_real_distribution<> pdistrib;
+        std::size_t m_width, m_height;
+    };
+}
 
 #endif // NAVIGATOR_H
---
+++ src/navigator.cpp
@@ -0,0 +1,41 @@
+#include "navigator.h"
+
+#include "point.h"
+
+namespace model
+{
+    Navigator::Navigator(std::size_t _width,
+                         std::size_t _height) :
+                                         eng(rd()),
+                                         distr(-1, 1),
+                                         offdistr(0, 2 * (_width + _height) - 4),
+                                         pdistrib(0, 1.0)
+    {
+        m_width = _width;
+        m_height = _height;
+    }
+
+    model::Point Navigator::setOnBorders()
+    {
+        //mapping all the point on the frame of the image
+        //to use a contiguous indexing system
+
+        //calculating a random index on the frame of the image
+        std::size_t offset = offdistr(eng);
+
+        //converting from that index to the actual x, y values for the random walker
+        model::Point walker = (offset < m_width) ? model::Point{offset, 0} :
+                  (m_width <= offset && offset < m_width + m_height) ? model::Point{m_width - 1, offset - m_width} :
+                  (m_width + m_height <= offset && offset < 2 * m_width + m_height) ?
+                                                                           model::Point{offset - m_width - m_height, m_height - 1} :
+                                                                           model::Point{0, offset - m_width - m_width - m_height};
+        return walker;
+
+    }
+
+    void Navigator::walk(model::Point& _walker)
+    {
+        _walker.x = _walker.x + distr(eng);
+        _walker.y = _walker.y + distr(eng);
+    }
+}
