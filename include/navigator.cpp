8ecd67bcbf1f68a5dcb6fa9a4065dc807ad71a2d
demiaster <marta.celeste.feriani@gmail.com>
2016-12-09 02:02:43 +0100

    new files: navigator.*

---
+++ include/navigator.cpp
@@ -0,0 +1,6 @@
+#include "navigator.h"
+
+Navigator::Navigator()
+{
+
+}
---
+++ include/navigator.h
@@ -0,0 +1,11 @@
+#ifndef NAVIGATOR_H
+#define NAVIGATOR_H
+
+
+class Navigator
+{
+public:
+    Navigator();
+};
+
+#endif // NAVIGATOR_H
