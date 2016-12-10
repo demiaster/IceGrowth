1cea36b1a185b2248d67095b9142eca1f0fad5b2
demiaster <marta.celeste.feriani@gmail.com>
2016-12-10 20:52:49 +0100

    icegenerator.cpp: implemented represent(), added debug calls, fixed dla_pattern()

--- src/icegenerator.cpp
+++ src/icegenerator.cpp
@@ -11,7 +11,7 @@
 #include<iostream>
 
 #define HITTEMPERATURE -100
-#define DIFFUSE_K 0.2
+#define DIFFUSE_K 0.002
 #define DIFFUSE_TIME 1.0
 #define RESET_TEMPERATURE 1.0
 
@@ -20,40 +20,62 @@ namespace controller
     //setup IceGenerator controller
 
     IceGenerator::IceGenerator(const std::size_t _width,
-                    const std::size_t _height) :
-        m_heatGrid(_width, _height),
-        m_iceGrid(_width, _height),
-        m_image(_width, _height)
+                    const std::size_t _height)
     {
         m_width = _width;
         m_height = _height;
-        setup();
     }
 
     void IceGenerator::setup()
     {
+        m_image.reset(new view::Image (m_width, m_height));
+        m_iceGrid.reset(new model::IceGrid(m_width, m_height));
+        m_heatGrid.reset(new model::HeatGrid(m_width, m_height));
         // reset the heatgrid to a custom temperature
-        m_heatGrid.reset(RESET_TEMPERATURE);
-        m_iceGrid.reset(0);
+        m_heatGrid->reset(RESET_TEMPERATURE);
+        m_heatGrid->inspect();
+        m_iceGrid->reset(0);
         // set the initial seed of the iceGrid + set the temperature of the seed inside the heatgrid
-        m_iceGrid.freeze(m_width / 2, m_height / 2, 1.0);
-        m_heatGrid.setTemperature(m_width / 2, m_height / 2, HITTEMPERATURE);
-        m_image.clearScreen(0, 0, 0);
+        m_iceGrid->freeze(m_width / 2, m_height / 2, 1.0);
+        m_heatGrid->setTemperature(m_width / 2, m_height / 2, -10);
+        m_heatGrid->inspect();
+        m_iceGrid->inspect();
+        m_image->clearScreen(0, 0, 0);
         return;
     }
 
     void IceGenerator::update()
     {
-        m_heatGrid.diffuse(DIFFUSE_K, DIFFUSE_TIME);
-        m_iceGrid.merge(m_heatGrid);
-        this->dla_pattern();
+        m_heatGrid->diffuse(DIFFUSE_K, DIFFUSE_TIME);
+        //to avoid melting
+        m_iceGrid->freeze(m_width / 2, m_height / 2, 1.0);
+        m_heatGrid->setTemperature(m_width / 2, m_height / 2, -10);
 
+        m_heatGrid->inspect();
+        m_iceGrid->merge(m_heatGrid.get());
+        this->dla_pattern();
+        m_heatGrid->inspect();
+        m_iceGrid->inspect();
         return;
     }
     void IceGenerator::represent()
     {
         //TODO: represent an iceGrid inside an image a framebuffer or a mesh
-
+        float temperature;
+        std::size_t ice;
+        for (std::size_t i = 0; i < m_width; ++i)
+        {
+            for (std::size_t j = 0; j < m_height; ++j)
+            {
+                temperature = m_heatGrid->getTemperature(i, j);
+                if (temperature < 0)
+                {
+                    ice = 255 - (temperature - HITTEMPERATURE)/(RESET_TEMPERATURE - HITTEMPERATURE) * 255;
+                    m_image->setPixel(i, j, 255, 255, 255);
+                }
+            }
+        }
+        m_image->save("represent_function.png");
         return;
     }
 
@@ -65,11 +87,21 @@ namespace controller
         while(true)
         {
             navigator.walk(random_walker);
-            std::cout << "x, y (" << random_walker.x << ", " << random_walker.y << ")\n";
-            PERCENTAGE probability = m_iceGrid.hit(random_walker.x, random_walker.y);
+            PERCENTAGE probability = m_iceGrid->hit(random_walker.x, random_walker.y);
+
+#ifdef TRACE
+            std::cout<<"IceGrid status\n";
+            m_iceGrid->inspect();
+            std::cout << "DLA Probability: (" << random_walker.x << ", " << random_walker.y << "), " <<
+              probability << "\n";
+#endif
             if(navigator.isFreezable(probability))
             {
-                m_iceGrid.freeze(random_walker.x, random_walker.y, probability);
+                m_iceGrid->freeze(random_walker.x, random_walker.y, probability);
+                m_heatGrid->setTemperature(random_walker.x, random_walker.y, HITTEMPERATURE);
+#ifdef DEBUG
+                std::cout << "Freeze: (" << random_walker.x << ", " << random_walker.y << ")\n";
+#endif
                 break;
             }
         }
