521fed31d7c2da1de13899fe6fa19409ae590833
demiaster <marta.celeste.feriani@gmail.com>
2016-12-10 12:22:32 +0100

    iceGenerator.cpp: removed commented code, reindented

--- src/icegenerator.cpp
+++ src/icegenerator.cpp
@@ -59,59 +59,37 @@ namespace controller
 
     void IceGenerator::dla_pattern()
     {
-        /*
-        std::uniform_int_distribution<> distr(-1, 1); // define the range
-        std::uniform_int_distribution<> offdistr(0, 2 * (m_width + m_height) - 4);
-        std::uniform_real_distribution<> pdistrib(0, 1.0);
-        */
         model::Navigator navigator (m_width, m_height);
+        model::Point random_walker = navigator.setOnBorder();
+        m_image.setPixel(random_walker.x, random_walker.y, 255, 0, 0);
 
-        //set seed
-        m_image.setPixel(m_width / 2, m_height / 2, 255, 255, 255);
-
-        for( int i = 0; i < 1000; ++i)
+        int j = 0;
+        while(true)
         {
-            //calculating a random index on the frame of the image
-            //std::size_t offset = offdistr(eng);
+            navigator.walk(random_walker);
+            std::cout << "x, y (" << random_walker.x << ", " << random_walker.y << ")\n";
 
-            //converting from that index to the actual x, y values for the random walker
-            /*model::Point random_walker = (offset < m_width) ? model::Point{offset, 0} :
-                                      (m_width <= offset && offset < m_width + m_height) ? model::Point{m_width - 1, offset - m_width} :
-                                      (m_width + m_height <= offset && offset < 2 * m_width + m_height) ?
-                                                                                model::Point{offset - m_width - m_height, m_height - 1} :
-                                                                                model::Point{0, offset - m_width - m_width - m_height};
+            //random_walker.x = random_walker.x + distr(eng);
+            //random_walker.y = random_walker.y + distr(eng);
 
-            */
-            model::Point random_walker = navigator.setOnBorders();
-            m_image.setPixel(random_walker.x, random_walker.y, 255, 0, 0);
-
-            int j = 0;
-            while(true)
+            //only check upper bounds because of
+            //Point unsigned int x, y coordinates
+            if(random_walker.x >= m_width || random_walker.y >= m_height)
             {
-               navigator.walk(random_walker);
-               std::cout << "x, y (" << random_walker.x << ", " << random_walker.y << ")\n";
-
-               //random_walker.x = random_walker.x + distr(eng);
-               //random_walker.y = random_walker.y + distr(eng);
-
-               //only check upper bounds because of
-               //Point unsigned int x, y coordinates
-               if(random_walker.x >= m_width || random_walker.y >= m_height)
-               {
-                   break;
-               }
-               //PERCENTAGE probability = m_iceGrid.hit(random_walker.x, random_walker.y);
-               else if(m_image.hit(random_walker.x, random_walker.y, 255, 255, 255))
-               {
-                   //ONLY TO TEST AGAIN NORMAL BEHAVIOUR
-                   //std::cout << "x, y (" << random_walker.x << ", " << random_walker.y << ")\n";
-                   m_image.setPixel(random_walker.x,
-                                  random_walker.y,
-                                  255, 255, 255);
-                   break;
-               }
-               //TODO: figure out why this breaks everything
-               /*if(probability > pdistrib(eng))
+                break;
+            }
+            //PERCENTAGE probability = m_iceGrid.hit(random_walker.x, random_walker.y);
+            else if(m_image.hit(random_walker.x, random_walker.y, 255, 255, 255))
+            {
+                //ONLY TO TEST AGAIN NORMAL BEHAVIOUR
+                //std::cout << "x, y (" << random_walker.x << ", " << random_walker.y << ")\n";
+                m_image.setPixel(random_walker.x,
+                                 random_walker.y,
+                                 255, 255, 255);
+                break;
+            }
+            //TODO: figure out why this breaks everything
+            /*if(probability > pdistrib(eng))
                {
                    m_iceGrid.freeze(random_walker.x, random_walker.y, probability);
                    m_image.setPixel(random_walker.x,
@@ -120,9 +98,8 @@ namespace controller
                    break;
                }
                */
-               ++j;
-               m_image.setPixel(random_walker.x, random_walker.y, 255 - (j % 255), 0, 0);
-            }
+            ++j;
+            m_image.setPixel(random_walker.x, random_walker.y, 255 - (j % 255), 0, 0);
         }
         m_image.save("dla_controller_navigator.png");
         return;
