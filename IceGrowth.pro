65e3b1c97ea706c44ac51d76e679477b411586fd
demiaster <marta.celeste.feriani@gmail.com>
2016-12-13 17:39:44 +0100

    new files: framebuffer.*

--- IceGrowth.pro
+++ IceGrowth.pro
@@ -19,15 +19,16 @@ SOURCES += src/main.cpp \
            src/heatgrid.cpp \
            src/icegrid.cpp \
            src/icegenerator.cpp \
-           src/navigator.cpp
+           src/navigator.cpp \
+           src/framebuffer.cpp
 HEADERS+= include/image.h \
     include/point.h \
     include/grid.h \
     include/heatgrid.h \
     include/icegrid.h \
     include/icegenerator.h \
-    include/navigator.h
-
+    include/navigator.h \
+    include/framebuffer.h
 
 macx:QMAKE_CXXFLAGS+=-DMAGICKCORE_HDRI_ENABLE=1 -DMAGICKCORE_QUANTUM_DEPTH=16 -DMAGICKCORE_HDRI_ENABLE=1 -DMAGICKCORE_QUANTUM_DEPTH=16 -DMAGICKCORE_HDRI_ENABLE=1 -DMAGICKCORE_QUANTUM_DEPTH=16 -I/usr/local/include/ImageMagick-6
 macx:LIBS+= -L/usr/local/lib -lMagick++-6.Q16 -lMagickWand-6.Q16 -lMagickCore-6.Q16
---
+++ include/framebuffer.h
@@ -0,0 +1,132 @@
+#ifndef FRAMEBUFFER_H_
+#define FRAMEBUFFER_H_
+
+#include <string>
+
+#ifdef __APPLE__
+  #include <unistd.h>
+  #define GLFW_INCLUDE_GLCOREARB
+#elif __linux__
+
+  #include <GL/glew.h>
+  #include <GL/gl.h>
+#endif
+#include <GLFW/glfw3.h>
+namespace frm
+{
+
+/**
+ * @brief      Generic framebuffer for visualizing images Written by Josh Bainbridge MSc 2015
+ * 
+ * Framebuffer that uses OpenGL and GLFW to visualize images. It will take a void pointer to data
+ * that can then be acceded within key callbacks externally. If a void pointer is not passed then
+ * default input is used. 
+ */
+class Framebuffer
+{
+public:
+  /**
+   * @brief      Initialiser list for class
+   */
+  Framebuffer()
+    : m_update(true)
+    , m_pan(false)
+    , m_resolution_x(700.f)
+    , m_resolution_y(500.f)
+    , m_screen_x(0.f)
+    , m_screen_y(0.f)
+    , m_state_x(0.f)
+    , m_state_y(0.f)
+    , m_trans_x(0.f)
+    , m_trans_y(0.f)
+    , m_scale(1.f)
+  {;}
+
+  /**
+   * @brief      Class destructor for deleting buffers and destroying window
+   */
+  ~Framebuffer();
+
+  /**
+   * @brief      Initialize framebuffer with parameters and optional control class
+   *
+   * @param[in]  _resx        buffers horizontal resolution
+   * @param[in]  _resy        buffers vertical resolution
+   * @param      _input_data  optional void pointer to class for mutation when using external callback
+   *
+   * @return     window pointer to be used to set external key callbacks
+   */
+  GLFWwindow* init(const size_t _resx, const size_t _resy, void* _input_data = NULL);
+
+  /**
+   * @brief      Bind data before drawing in case of use with other contexts
+   */
+  void bind();
+
+  /**
+   * @brief      Draw image to framebuffer
+   */
+  void draw();
+
+  /**
+   * @brief      Test if window should close
+   *
+   * @return     boolean value
+   */
+  bool close();
+
+  /**
+   * @brief      Poll for changed events
+   */
+  void poll();
+
+  /**
+   * @brief      Set location of image data to be passed onto gpu
+   *
+   * @param[in]  _image     pointer to image data
+   * @param[in]  _resx      horizontal resolution
+   * @param[in]  _resy      vertical resolution
+   */
+  void image(const unsigned char* _image, const size_t _resx, const size_t _resy);
+
+  /**
+   * @brief      Set framebuffer window title to string
+   *
+   * @param[in]  _title  title
+   */
+  void title(const std::string &_title);
+  bool shouldClose(){return glfwWindowShouldClose(m_window);}
+
+private:
+  GLFWwindow* m_window;
+  GLuint m_texture;
+  GLuint m_shader_program;
+  GLuint m_vertex_shader;
+  GLuint m_fragment_shader;
+  GLuint m_vbo;
+  GLuint m_vao;
+  GLint m_pos_attrib;
+  GLint m_tex_attrib;
+  GLint m_trans_uniform;
+  GLint m_scale_uniform;
+
+  void createContext();
+  void createSurface();
+  static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+  static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+  static void cursorPositionCallback(GLFWwindow* window, double xpos, double ypos);
+  static void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+
+private:
+  bool m_update;
+  bool m_pan;
+  float m_resolution_x, m_resolution_y;
+  float m_screen_x, m_screen_y;
+  float m_state_x, m_state_y;
+  float m_trans_x, m_trans_y;
+  float m_scale;
+};
+
+}
+
+#endif
