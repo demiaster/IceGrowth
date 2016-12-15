349a742dd01273bb5cccd09120257545dd49c045
demiaster <marta.celeste.feriani@gmail.com>
2016-12-15 13:05:58 +0100

    randomdist.h: implemented

--- include/randomdist.h
+++ include/randomdist.h
@@ -1,12 +1,26 @@
 #ifndef COMMON_RANDOMDIST_H
-#define COMMON_RANDOM_DIST_H
+#define COMMON_RANDOMDIST_H
 
+#include <random>
 namespace common
 {
     class RandomDist
     {
     public:
-    private:
+    protected:
+        inline RandomDist(const std::size_t _width,
+                          const std::size_t _height) :
+                                            eng(rd()),
+                                            distr(-1, 1),
+                                            offdistr(0, 2 * (_width + _height) - 4),
+                                            pdistrib(0, 1.0) {;}
+        std::random_device rd; // obtain a random number from hardware
+        std::mt19937 eng; // seed the generator
+        std::uniform_int_distribution<> distr;
+        std::uniform_int_distribution<> offdistr;
+        std::uniform_real_distribution<> pdistrib;
 
     };
 }
+
+#endif //COMMON_RANDOMDIST_H
