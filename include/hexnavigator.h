e425b70d29aa092a755e8aec99325d8e3b68eb86
demiaster <marta.celeste.feriani@gmail.com>
2016-12-15 13:07:57 +0100

    haxnavigator.h: declared methods

--- include/hexnavigator.h
+++ include/hexnavigator.h
@@ -1,12 +1,18 @@
-#ifdef MODEL_HEXNAVIGATOR_H
+#ifndef MODEL_HEXNAVIGATOR_H
 #define MODEL_HEXNAVIGATOR_H
 
 #include "navigator.h"
-
+#include "randomdist.h"
 namespace model
 {
-    class HexNavigator : public Navigator
+    class HexNavigator : public Navigator, common::RandomDist
     {
-
+    public:
+        HexNavigator(const std::size_t _width, const std::size_t _height);
+        model::Point setOnBorder() override;
+        void walk(model::Point& _walker) override;
+        //void diffuseOnAxis() override;
+        bool isFreezable(const float _probability); 
     };
 }
+#endif //MODEL_HEXNAVIGATOR_H
