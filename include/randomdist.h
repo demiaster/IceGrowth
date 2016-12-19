4df242672afa16f05381123f5aaa914d5b53f6cf
demiaster <marta.celeste.feriani@gmail.com>
2016-12-19 14:51:40 +0100

    randomdist.h: added getter, refactoring

--- include/randomdist.h
+++ include/randomdist.h
@@ -1,24 +1,36 @@
 #ifndef COMMON_RANDOMDIST_H
 #define COMMON_RANDOMDIST_H
 
+#define GETTER(type, variable) \
+    inline type get_ ## variable  () { \
+        return m_ ## variable  (m_eng); \
+    }
+
 #include <random>
 namespace common
 {
     class RandomDist
     {
     public:
-    protected:
         inline RandomDist(const std::size_t _width,
                           const std::size_t _height) :
-                                            eng(rd()),
-                                            distr(-1, 1),
-                                            offdistr(0, 2 * (_width + _height) - 4),
-                                            pdistrib(0, 1.0) {;}
-        std::random_device rd; // obtain a random number from hardware
-        std::mt19937 eng; // seed the generator
-        std::uniform_int_distribution<> distr;
-        std::uniform_int_distribution<> offdistr;
-        std::uniform_real_distribution<> pdistrib;
+                                            m_eng(m_rd()),
+                                            m_distr(-1, 1),
+                                            m_offdistr(0, 2 * (_width + _height) - 4),
+                                            m_pdistrib(0, 1.0) {;}
+
+        GETTER(std::size_t, distr)
+
+        GETTER(std::size_t, offdistr)
+
+        GETTER(float, pdistrib)
+
+    private:
+        std::random_device m_rd; // obtain a random number from hardware
+        std::mt19937 m_eng; // seed the generator
+        std::uniform_int_distribution<> m_distr;
+        std::uniform_int_distribution<> m_offdistr;
+        std::uniform_real_distribution<> m_pdistrib;
 
     };
 }
