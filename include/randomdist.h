23b03117e77ed786744f26b35c3fcbf1ca6ce601
demiaster <marta.celeste.feriani@gmail.com>
2016-12-20 16:51:04 +0100

    randomdist.h: implemented using macro

--- include/randomdist.h
+++ include/randomdist.h
@@ -1,38 +1,25 @@
 #ifndef COMMON_RANDOMDIST_H
 #define COMMON_RANDOMDIST_H
 
-#define GETTER(type, variable) \
-    inline type get_ ## variable  () { \
-        return m_ ## variable  (m_eng); \
-    }
-
 #include <random>
 namespace common
 {
-    class RandomDist
-    {
-    public:
-        inline RandomDist(const std::size_t _width,
-                          const std::size_t _height) :
-                                            m_eng(m_rd()),
-                                            m_distr(-1, 1),
-                                            m_offdistr(0, 2 * (_width + _height) - 4),
-                                            m_pdistrib(0, 1.0) {;}
+#define RANDOMDIST(name, distr, type) \
+    class name \
+    { \
+    public: \
+        inline name(const type low, const type high) : m_eng(m_rd()), m_distr(low, high) {}; \
+        inline type get_distr() { \
+            return m_distr(m_eng); \
+        } \
+    private: \
+        std::random_device m_rd; \
+        std::mt19937 m_eng;  \
+        std::uniform_ ## distr ## _distribution<> m_distr; \
+    }
 
-        GETTER(std::size_t, distr)
-
-        GETTER(std::size_t, offdistr)
-
-        GETTER(float, pdistrib)
-
-    private:
-        std::random_device m_rd; // obtain a random number from hardware
-        std::mt19937 m_eng; // seed the generator
-        std::uniform_int_distribution<> m_distr;
-        std::uniform_int_distribution<> m_offdistr;
-        std::uniform_real_distribution<> m_pdistrib;
-
-    };
+    RANDOMDIST(IntDistribution, int, std::size_t);
+    RANDOMDIST(FloatDistribution, real, float);
 }
 
 #endif //COMMON_RANDOMDIST_H
