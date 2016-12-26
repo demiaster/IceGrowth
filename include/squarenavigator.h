f36cf13a8e89d7af4d93197842a814356c6cb546
demiaster <marta.celeste.feriani@gmail.com>
2016-12-27 00:28:27 +0100

    squarenavigator.h: added TUPLE, refactoring

--- include/squarenavigator.h
+++ include/squarenavigator.h
@@ -2,9 +2,13 @@
 #define MODEL_SQUARENAVIGATOR_H
 
 #include <random>
+#include <tuple>
+#include <array>
 #include "point.h"
 #include "navigator.h"
 #include "randomdist.h"
+
+#define TUPLE std::tuple<DiffPoint, DiffPoint>
 
 namespace model
 {
@@ -15,13 +19,12 @@ namespace model
                         const std::size_t _height);
         model::Point setOnBorder() override;
         void walk(model::Point& _walker) override;
-        //void diffuseOnAxis() override;
-        //bool isFreezable(const float _probability);
+        void diffuseOnAxis() override;
     private:
         common::IntDistribution m_borderdist, m_neighboursdist;
 
         //TODO: make this variable static again
-        DiffPoint m_point[8] =
+        DiffPoint m_neighbour[8] =
                         {{-1, -1},
                           {0, -1},
                           {1, -1},
@@ -30,6 +33,10 @@ namespace model
                           {0, 1},
                           {-1, 1},
                           {-1, 0}};
+
+        std::array<TUPLE, 2> m_axis =
+                                    {{TUPLE({-1, 0}, {1, 0}),
+                                      TUPLE({0, -1}, {0, 1})}};
     };
 }
 
