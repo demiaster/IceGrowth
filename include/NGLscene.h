fbff8fec31f3c154c7d001fa62a003460b8d432a
demiaster <marta.celeste.feriani@gmail.com>
2016-12-27 12:23:26 +0100

    new file: NGLscene.h

---
+++ include/NGLscene.h
@@ -0,0 +1,14 @@
+#ifndef NGLSCENE_H
+#define NGLSCENE_H
+
+#include <QOpenGLWindow>
+
+namespace view
+{
+    class NGLScene : public QOpenGLWindow
+    {
+    public:
+    private:
+    };
+}
+#endif //NGLSCENE_H
