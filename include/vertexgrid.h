aa0233b0971535f572ae6ffe07cbfcc0978e079f
demiaster <marta.celeste.feriani@gmail.com>
2017-01-22 13:09:27 +0100

    new class: vertexgrid.h

---
+++ include/vertexgrid.h
@@ -0,0 +1,15 @@
+#ifndef VERTEXGRID_H
+#define VERTEXGRID_H
+
+#include "grid.h"
+#include "point.h"
+
+namespace view
+{
+    class vertexgrid : public model::Grid<model::Vertex, 2>
+    {
+    public:
+    private:
+    };
+}
+#endif //VERTEXGRID_H
