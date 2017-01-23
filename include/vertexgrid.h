26dedb41b7e461145ee75c8dc0f32113a3ebd50e
demiaster <marta.celeste.feriani@gmail.com>
2017-01-23 12:15:33 +0100

    vertexgrid.h: added members, ctor definition

--- include/vertexgrid.h
+++ include/vertexgrid.h
@@ -1,3 +1,4 @@
+#define VERTEXGRID_H 1
 #ifndef VERTEXGRID_H
 #define VERTEXGRID_H
 
@@ -6,10 +7,14 @@
 
 namespace view
 {
-    class vertexgrid : public model::Grid<model::Vertex, 2>
+    class VertexGrid : public model::Grid<model::Vertex, 2>
     {
-    public:
-    private:
+        public:
+            //sostituire con una variabile fatta bene
+            VertexGrid(const std::size_t _width * 21,
+                       const std::size_t _height);
+        private:
+            std::size_t m_width, m_height;
     };
 }
 #endif //VERTEXGRID_H
