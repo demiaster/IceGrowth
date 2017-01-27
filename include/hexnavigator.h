d20233a05480ecc6a2a3de2d0ad793dceecedc33
demiaster <marta.celeste.feriani@gmail.com>
2017-01-27 23:04:35 +0100

    hexnavigator.h: removed onAxis()

--- include/hexnavigator.h
+++ include/hexnavigator.h
@@ -36,29 +36,6 @@ namespace model
           }
         }
 
-        template <typename OPERATION>
-        void onAxis(model::Point& _point, OPERATION operation)
-        {
-            std::array<TUPLE, 3>& m_axis = _point.x % 2 ? m_axis_even : m_axis_odd;
-//            std::cout<< "insideonAxis"<<std::endl;
-            for (std::size_t i = 0; i < 3; i++)
-            {
-              model::Point left = _point,
-                           right = _point;
-              left.x  += std::get<0>(m_axis[i]).x;
-              left.y  += std::get<0>(m_axis[i]).y;
-              right.x += std::get<1>(m_axis[i]).x;
-              right.y += std::get<1>(m_axis[i]).y;
-
-              if (0 <= left.x && left.x < m_width &&
-                  0 <= left.y && left.y < m_height &&
-                  0 <= right.x && right.x < m_width &&
-                  0 <= right.y && right.y < m_height) {
-                  operation(left, _point, right);
-              }
-            }
-        }
-        //common::RandomDist& m_randomdist;
         common::IntDistribution m_borderdist, m_neighboursdist;
 
         // for even column index
