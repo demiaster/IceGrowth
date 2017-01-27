c935abe6ba47d7e46efee2f3127a1dc2d9eead0b
demiaster <marta.celeste.feriani@gmail.com>
2017-01-27 23:04:12 +0100

    squarenavigator.cpp: removed onAxis()

--- include/squarenavigator.h
+++ include/squarenavigator.h
@@ -35,26 +35,6 @@ namespace model
           }
         }
 
-        template <typename OPERATION>
-        void onAxis(model::Point& _point, OPERATION operation)
-        {
-          for (std::size_t i = 0; i < 2; i++)
-          {
-            model::Point left = _point,
-                         right = _point;
-            left.x  += std::get<0>(m_axis[i]).x;
-            left.y  += std::get<0>(m_axis[i]).y;
-            right.x += std::get<1>(m_axis[i]).x;
-            right.y += std::get<1>(m_axis[i]).y;
-
-            if (0 <= left.x && left.x < m_width &&
-                0 <= left.y && left.y < m_height &&
-                0 <= right.x && right.x < m_width &&
-                0 <= right.y && right.y < m_height) {
-                operation(left, _point, right);
-            }
-          }
-        }
     private:
         common::IntDistribution m_borderdist, m_neighboursdist;
 
